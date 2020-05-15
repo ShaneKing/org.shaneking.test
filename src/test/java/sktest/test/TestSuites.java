e616f31c6f4719895634acd455b535717171154b
ShaneKing <github@azolla.org>
2020-05-15 21:21:37 +0800

    adjust some project dep

--- src/test/java/sktest/test/TestSuites.java
+++
@@ -1,9 +0,0 @@
-package sktest.test;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({SKUnitTest.class})
-public class TestSuites {
-}

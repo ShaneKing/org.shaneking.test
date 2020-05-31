70ce264aa8928cccb3a9d41d2ba090b87ced9483
ShaneKing <github@azolla.org>
2020-06-01 06:46:17 +0800

    0.13.2 org.shaneking.test add skFileClass for inherit

--- src/main/java/org/shaneking/test/SKUnit.java
+++ src/main/java/org/shaneking/test/SKUnit.java
@@ -11,58 +11,64 @@ import org.shaneking.skava.util.Regex0;
 import java.io.File;
 
 public class SKUnit {
-    public static final File MAVEN_TEST_ROOT_FOLDER = new File("src/test/java");
-    @Rule
-    public TestName testName = new TestName();
-    private Stopwatch stopwatch = Stopwatch.createStarted();
+  public static final File MAVEN_TEST_ROOT_FOLDER = new File("src/test/java");
+  @Rule
+  public TestName testName = new TestName();
+  private Stopwatch stopwatch = Stopwatch.createStarted();
 
-    //CFG BEGIN:prepare
-    public File skTestFolder(Class clazz) {
-        return new File(MAVEN_TEST_ROOT_FOLDER, clazz.getName().replaceAll(clazz.getSimpleName(), "testfiles").replaceAll(Regex0.DOT, String0.SLASH));
-    }
+  //CFG BEGIN:prepare
+  public File skTestFolder(Class clazz) {
+    Class realClass = skFileClass(clazz);
+    return new File(MAVEN_TEST_ROOT_FOLDER, realClass.getName().replaceAll(realClass.getSimpleName(), "testfiles").replaceAll(Regex0.DOT, String0.SLASH));
+  }
 
-    public File skTestFiles(Class clazz, String seq, String io, String fileType) {
-        return new File(skTestFolder(clazz), clazz.getSimpleName() + String0.UNDERLINE + testName.getMethodName() + String0.UNDERLINE + seq + String0.UNDERLINE + io + String0.DOT + fileType);
-    }
+  public File skTestFiles(Class clazz, String seq, String io, String fileType) {
+    Class realClass = skFileClass(clazz);
+    return new File(skTestFolder(realClass), realClass.getSimpleName() + String0.UNDERLINE + testName.getMethodName() + String0.UNDERLINE + seq + String0.UNDERLINE + io + String0.DOT + fileType);
+  }
 
-    public File skTestIFiles(Class clazz, String seq, String fileType) {
-        return skTestFiles(clazz, seq, "i", fileType);
-    }
+  public File skTestIFiles(Class clazz, String seq, String fileType) {
+    return skTestFiles(skFileClass(clazz), seq, "i", fileType);
+  }
 
-    public File skTestOFiles(Class clazz, String seq, String fileType) {
-        return skTestFiles(clazz, seq, "o", fileType);
-    }
+  public File skTestOFiles(Class clazz, String seq, String fileType) {
+    return skTestFiles(skFileClass(clazz), seq, "o", fileType);
+  }
 
-    public File skTestIFiles(String seq, String fileType) {
-        return skTestIFiles(this.getClass(), seq, fileType);
-    }
+  public File skTestIFiles(String seq, String fileType) {
+    return skTestIFiles(this.getClass(), seq, fileType);
+  }
 
-    public File skTestOFiles(String seq, String fileType) {
-        return skTestOFiles(this.getClass(), seq, fileType);
-    }
+  public File skTestOFiles(String seq, String fileType) {
+    return skTestOFiles(this.getClass(), seq, fileType);
+  }
 
-    public File skTestIFiles(String fileType) {
-        return skTestIFiles(null, fileType);
-    }
+  public File skTestIFiles(String fileType) {
+    return skTestIFiles(null, fileType);
+  }
 
-    public File skTestOFiles(String fileType) {
-        return skTestOFiles(null, fileType);
-    }
-    //CFG END:prepare
+  public File skTestOFiles(String fileType) {
+    return skTestOFiles(null, fileType);
+  }
 
-    //CFG BEGIN:watch
-    @Before
-    public void setUp() {
-        stopwatch = Stopwatch.createStarted();
-    }
+  public Class skFileClass(Class clazz) {
+    return clazz;
+  }
+  //CFG END:prepare
 
-    @After
-    public void tearDown() {
-        System.out.println(testName.getMethodName() + String0.EQUAL + stopwatch.stop());
-    }
-    //CFG END:watch
+  //CFG BEGIN:watch
+  @Before
+  public void setUp() {
+    stopwatch = Stopwatch.createStarted();
+  }
 
-    public void skPrint(Object o) {
-        System.out.println(testName.getMethodName() + String0.EQUAL + o);
-    }
+  @After
+  public void tearDown() {
+    System.out.println(testName.getMethodName() + String0.EQUAL + stopwatch.stop());
+  }
+  //CFG END:watch
+
+  public void skPrint(Object o) {
+    System.out.println(testName.getMethodName() + String0.EQUAL + o);
+  }
 }

4fe19d939ddf16043cfb18497de7236a609fde3e
ShaneKing <github@azolla.org>
2020-06-03 00:43:50 +0800

    0.13.6 optimizing org.shaneking.test

--- src/main/java/org/shaneking/test/SKUnit.java
+++ src/main/java/org/shaneking/test/SKUnit.java
@@ -3,6 +3,7 @@ package org.shaneking.test;
 import com.google.common.base.Stopwatch;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -12,23 +13,34 @@ import org.shaneking.skava.util.Regex0;
 
 import java.io.File;
 
+@Slf4j
 public class SKUnit {
   public static final File MAVEN_TEST_ROOT_FOLDER = new File("src/test/java");
 
   @Rule
-  public TestName testName = new TestName();
+  public TestName testName = new TestName();//Can't private
+  @Getter
+  @Setter
   private Stopwatch stopwatch = Stopwatch.createStarted();
-
   @Getter
   @Setter
   private Class tstClazz = this.getClass();
   @Getter
   @Setter
   private String tstSeq = null;
+  @Getter
+  @Setter
+  private String tstFiles = "tstfiles";
+  @Getter
+  @Setter
+  private String tstI = String0.I.toLowerCase();
+  @Getter
+  @Setter
+  private String tstO = String0.O.toLowerCase();
 
-  //CFG BEGIN:prepare
+  //CFG BEGIN:files
   public File tstFolder() {
-    return new File(MAVEN_TEST_ROOT_FOLDER, this.getTstClazz().getName().replaceAll(this.getTstClazz().getSimpleName(), "tstfiles").replaceAll(Regex0.DOT, String0.SLASH));
+    return new File(MAVEN_TEST_ROOT_FOLDER, this.getTstClazz().getName().replaceAll(this.getTstClazz().getSimpleName(), getTstFiles()).replaceAll(Regex0.DOT, String0.SLASH));
   }
 
   public File tstFiles(String io, String fileType) {
@@ -36,27 +48,35 @@ public class SKUnit {
   }
 
   public File tstIFiles(String fileType) {
-    return tstFiles("i", fileType);
+    return tstFiles(getTstI(), fileType);
   }
 
   public File tstOFiles(String fileType) {
-    return tstFiles("o", fileType);
+    return tstFiles(getTstO(), fileType);
   }
-  //CFG END:prepare
+  //CFG END:files
 
   //CFG BEGIN:watch
   @Before
   public void setUp() {
-    stopwatch = Stopwatch.createStarted();
+    tstSetUp();
+  }
+
+  public void tstSetUp() {
+    setStopwatch(Stopwatch.createStarted());
   }
 
   @After
   public void tearDown() {
-    System.out.println(testName.getMethodName() + String0.EQUAL + stopwatch.stop());
+    tstTearDown();
+  }
+
+  public void tstTearDown() {
+    log.info(testName.getMethodName() + String0.EQUAL + this.getStopwatch().stop());
   }
   //CFG END:watch
 
   public void tstPrint(Object o) {
-    System.out.println(testName.getMethodName() + String0.EQUAL + o);
+    log.info(testName.getMethodName() + String0.EQUAL + o);
   }
 }
